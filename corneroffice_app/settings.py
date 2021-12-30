063ea7dcad08830331c8f49003e04833a45655fa
Eric Torigian <eric@torigian.com>
2021-12-30 13:05:38 -0500

    updated settings.py

--- corneroffice_app/settings.py
+++ corneroffice_app/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenose.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'corneroffice_app.urls'
