ff96a09b1753411af09cfe0ab8957916b2633e77
Eric Torigian <eric@torigian.com>
2021-12-31 07:41:48 -0500

    created clients model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Clients(models.Model):
+    contact = models.CharField(max_length = 200)
+    company = models.CharField(max_length = 200)
+    source = models.CharField(max_length = 200)
+    status = models.CharField(max_length = 200)
+    comments = models.TextField()
+    #auto time fields
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'base.html', context)
+
+
+def clients(request):
+    clients = {"Safran Cabin", "Connections", "Stellanis", "Other co"}
+
+    context = {'clients': clients }
+    return render(request, 'clients.html', context)
+
